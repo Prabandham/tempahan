72a3c1fc11b941bca8209abe81f572035d3084e6
Srinidhi Prabandham <srinidhi.prabandham@icicletech.com>
2018-04-23 20:56:23 +0530

    [Shifts] adding endtime validation

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,3 +1,11 @@
 class Shift < ApplicationRecord
   belongs_to :restaturant
+
+  validate :end_time_should_be_greater_than_start_time
+
+  def end_time_should_be_greater_than_start_time
+    if start_time >= end_time
+      errors.add(:end_time, "must be greater than start_time")
+    end
+  end
 end
