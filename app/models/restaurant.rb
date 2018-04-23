bb4ebe1f1fb06bad13ead6dc378d4d368edcd218
Srinidhi Prabandham <srinidhi.prabandham@icicletech.com>
2018-04-23 17:53:13 +0530

    adding associations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,6 @@ class Restaurant < ApplicationRecord
   validates :email, :phone, uniqueness: true
   validates :email, format: { with: /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/ } # May be good to move this to a concern
   validates :phone, format: { with: /\A\+603\s\d{4}\s\d{4}\z/ } # Took +603 2713 0485 as reference.
+
+  has_many :shifts, :tables
 end
